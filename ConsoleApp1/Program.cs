8971e3504ba3886a952a566cde4827c7ea61d26f
Alex Watts <alex.watts@maptek-au.local>
2018-10-29 17:07:09 +1100

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine($"disco.Error: {disco.Error}");
+                Console.ReadLine();
+
+                return;
+            }
+
+            // request token
+            //var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "511536EF-F270-4058-80CA-1C89C192F69A");
+            //var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "alice", "api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine($"tokenResponse.Error: {tokenResponse.Error}");
+                Console.ReadLine();
+
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:50419/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"response.StatusCode: {response.StatusCode}");
+                Console.ReadLine();
+
+                return;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(JArray.Parse(content));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ is4inmem/Startup.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using IdentityServer4;
+using IdentityServer4.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace is4inmem
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //services.Configure<IISOptions>(options =>
+            //{
+            //    options.AutomaticAuthentication = false;
+            //    options.AuthenticationDisplayName = "Windows";
+            //});
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddTestUsers(TestUsers.Users);
+
+            // in-memory, code config
+            //builder.AddInMemoryIdentityResources(Config.GetIdentityResources());
+            //builder.AddInMemoryApiResources(Config.GetApis());
+            //builder.AddInMemoryClients(Config.GetClients());
+
+            // in-memory, json config
+            builder.AddInMemoryIdentityResources(Configuration.GetSection("IdentityResources"));
+            builder.AddInMemoryApiResources(Configuration.GetSection("ApiResources"));
+            builder.AddInMemoryClients(Configuration.GetSection("clients"));
+            var webappPort = "53425";
+
+            var list = new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+                new Client
+                {
+                    ClientId = "webapp",
+                    ClientName = "MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:53425/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+
+            builder.AddInMemoryClients(list);
+
+            builder.AddDeveloperSigningCredential();
+
+            //if (Environment.IsDevelopment())
+            //{
+            //    builder.AddDeveloperSigningCredential();
+            //}
+            //else
+            //{
+            //    throw new Exception("need to configure key material");
+            //}
+
+            //services.AddAuthentication()
+            //    .AddGoogle(options =>
+            //    {
+            //        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+            //        options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+            //        options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+            //    });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
